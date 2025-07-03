a0c07418a9e9c4a3eedb1c8d175b9c8bb9f98d49
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2025-07-02 23:54:27 -0500

    New homeworks

---
+++ greeting_function.py
@@ -0,0 +1,7 @@
+def greeting(name):
+    greeting_message = f"Hello, {name}"
+    return greeting_message
+
+result = greeting("Pedro")
+
+print(result)
---
+++ prueba.py
@@ -0,0 +1,7 @@
+variable1 = 0
+def funcion1(variable1):
+    variable2 = 100
+    print("Variable 1:", variable1)
+    print("Variable 2:", variable2)
+    
+funcion1(45)
